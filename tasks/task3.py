f872bbab42103becaab9aad74d48607404f7636a
Kvik2 <kvik_222@mail.ru>
2025-12-09 11:15:22 +0500

    1

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+    s = input()
+    c = input()
+
+    print((s == "awesome") or (c == "awesome"))    
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
